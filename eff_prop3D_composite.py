064d88ea19690129d7001e50ad51736fcac1c840
SvetlanaErmakova <Svetlana.lit1533@gmail.com>
2024-03-21 14:50:32 +0300

    Class(eff_prop3D)
    Now the task is a class, private methods have been added to it, 2 functions available to the user - .find_Keff() .find_alpha(), and there is also a decorator with which you can calculate the operating time of these 2 functions. The cupy library is still used for parallelization.

--- eff_prop3D_composite.py
+++ eff_prop3D_composite.py
@@ -1,249 +1,305 @@
-import cupy as cp
-import numpy as np
 import time
 
-def av4_xy(A):
-    return 0.25 * (A[:-1, :-1, :] + A[:-1, 1:, :] + A[1:, :-1, :] + A[1:, 1:, :])
-def av4_xz(A):
-    return 0.25*(A[:-1, :, :-1] + A[:-1, :, 1:] + A[1:, :, :-1] + A[1:, :, 1:])
-def av4_yz(A):
-    return 0.25*(A[:, :-1, :-1] + A[:, :-1, 1:] + A[:, 1:, :-1] + A[:, 1:, 1:])
-#
-start_numpy = time.time()
+import cupy as cp
+import numpy as np
 
-# PHYSICS
-Lx = 1.0
-Ly = 1.0
-Lz = 1.0
+def timer(func):
+    def wrapper(self, *args, **kwargs):
+        start_time = time.time()
+        result = func(self, *args, **kwargs)
+        end_time = time.time()
+        print(f"Метод {func.__name__} работал {end_time - start_time:.2f} секунд")
+        return result
+    return wrapper
 
-# E0n = 10.0
-# nu0n = 0.25
-# alp0n = 1.3e-5
-E0n = 0.0
-nu0n = 0.0
-alp0n = 0.0
+class eff_prop3D():
 
-E0p = 1.0
-nu0p = 0.4
-alp0p = 7.7e-5
+  def __init__(self, size, grid_points, CFL, rho, E0p, nu0p, alp0p, E0n, nu0n, alp0n, radios,
+                stress_initial_cond, U_initial_cond, V_initial_cond, T, strain_bound_cond):
+      #size
+      self.Lx=size[0]
+      self.Ly=size[1]
+      self.Lz=size[2]
 
-if E0p>E0n:
-    E00=E0p
-    nu00=nu0p
-else:
-    E00=E0n
-    nu00=nu0n
+      #grid
+      self.Nx=grid_points[0]
+      self.Ny=grid_points[1]
+      self.Nz=grid_points[2]
+      self.Nt=grid_points[3]
+      #CFL
+      self.CFL=CFL
+      #preprocessing
+      self.__fill_preprocessing(E0p, nu0p, E0n, nu0n, rho)
+      #material
+      self.__fill_material(rho, E0p, nu0p, alp0p, E0n, nu0n, alp0n, radios)
+      #initial condition
+      self.set_initial_cond( stress_initial_cond, U_initial_cond, V_initial_cond, T)
+      #boundary condition
+      self.set_boundary_cond(strain_bound_cond )
 
-rho = 1.0
-K0 = E00 / (3.0 * (1 - 2 * nu00))
-G0 = E00 / (2.0 + 2.0 * nu00)
+  def __fill_preprocessing(self, E0p, nu0p, E0n, nu0n, rho):
+      #для dt
+      if E0p>E0n:
+          E00=E0p
+          nu00=nu0p
+      else:
+          E00=E0n
+          nu00=nu0n
 
-# NUMERICS
-Nx = 100
-Ny = 100
-Nz = 100
+      self.K0 = E00 / (3.0 * (1 - 2 * nu00))
+      self.G0 = E00 / (2.0 + 2.0 * nu00)
+      #
+      dX = self.Lx / (self.Nx - 1)
+      dY = self.Ly / (self.Ny - 1)
+      dZ = self.Lz / (self.Nz - 1)
 
-Nt = 1000
-CFL = 0.25
+      x = cp.linspace(-self.Lx/2, self.Lx/2, self.Nx)
+      y = cp.linspace(-self.Ly/2, self.Ly/2, self.Ny)
+      z = cp.linspace(-self.Lz/2, self.Lz/2, self.Nz)
+      self.x, self.y, self.z = cp.meshgrid(x, y, z)
 
-# PREPROCESSING
-dX = Lx / (Nx - 1)
-dY = Ly / (Ny - 1)
-dZ = Lz / (Nz - 1)
-x = cp.linspace(-Lx/2, Lx/2, Nx)
-y = cp.linspace(-Ly/2, Ly/2, Ny)
-z = cp.linspace(-Lz/2, Lz/2, Nz)
-x, y, z = cp.meshgrid(x, y, z)
+      self.xUx, self.yUx, self.zUx = cp.meshgrid( cp.linspace(-(self.Lx + dX)/2, (self.Lx + dX)/2, self.Nx+1),
+                                  cp.linspace(-self.Ly/2, self.Ly/2, self.Ny),
+                                  cp.linspace(-self.Lz/2, self.Lz/2, self.Nz), indexing='ij')
 
-xUx, yUx, zUx = cp.meshgrid( cp.linspace(-(Lx + dX)/2, (Lx + dX)/2, Nx+1),
-                             cp.linspace(-Ly/2, Ly/2, Ny),
-                             cp.linspace(-Lz/2, Lz/2, Nz), indexing='ij')
+      self.xUy, self.yUy, self.zUy = cp.meshgrid( cp.linspace(-self.Lx/2, self.Lx/2, self.Nx),
+                                  cp.linspace(-(self.Ly + dY)/2, (self.Ly + dY)/2, self.Ny+1),
+                                  cp.linspace(-self.Lz/2, self.Lz/2, self.Nz), indexing='ij')
 
-xUy, yUy, zUy = cp.meshgrid( cp.linspace(-Lx/2, Lx/2, Nx),
-                             cp.linspace(-(Ly + dY)/2, (Ly + dY)/2, Ny+1),
-                             cp.linspace(-Lz/2, Lz/2, Nz), indexing='ij')
+      self.xUz, self.yUz, self.zUz = cp.meshgrid(cp.linspace(-self.Lx/2, self.Lx/2, self.Nx),
+                                  cp.linspace(-self.Ly/2, self.Ly/2, self.Ny),
+                                  cp.linspace(-(self.Lz + dZ)/2, (self.Lz + dZ)/2, self.Nz+1), indexing='ij')
+      self.dt = self.CFL * min(dX, min(dY, dZ)) / cp.sqrt((self.K0 + 4*self.G0/3) / rho)
+      self.damp = 4 / self.dt / self.Nx
 
-xUz, yUz, zUz = cp.meshgrid(cp.linspace(-Lx/2, Lx/2, Nx),
-                             cp.linspace(-Ly/2, Ly/2, Ny),
-                             cp.linspace(-(Lz + dZ)/2, (Lz + dZ)/2, Nz+1), indexing='ij')
+      self.dX=dX
+      self.dY=dY
+      self.dZ=dZ
 
+  def __fill_material(self, rho, E0p, nu0p, alp0p, E0n, nu0n, alp0n, radios):
+      self.rho = rho
+      #
+      E = E0p * cp.ones((self.Nx, self.Ny, self.Nz))
+      nu = nu0p * cp.ones((self.Nx, self.Ny, self.Nz))
+      alp = alp0p * cp.ones((self.Nx, self.Ny, self.Nz))
+      indices = cp.sqrt(self.x**2 + self.y**2 + self.z**2) < radios
+      E[indices] = E0n
+      nu[indices] = nu0n
+      alp[indices] = alp0n
+      K = E / (3.0 * (1 - 2 * nu))
+      G = E / (2.0 + 2.0 * nu)
 
-dt = CFL * min(dX, min(dY, dZ)) / cp.sqrt((K0 + 4*G0/3) / rho)
-damp = 4 / dt / Nx
+      self.E=E
+      self.nu=nu
+      self.alp=alp
+      self.K=K
+      self.G=G
 
-# MATERIALS
+  def set_initial_cond(self, stress_initial_cond, U_initial_cond, V_initial_cond, T):
+      self.P0=stress_initial_cond[0]
+      self.tauxx=stress_initial_cond[1]
+      self.tauyy=stress_initial_cond[2]
+      self.tauzz=stress_initial_cond[3]
 
-E = E0p * cp.ones((Nx, Ny, Nz))
-nu = nu0p * cp.ones((Nx, Ny, Nz))
-alp = alp0p * cp.ones((Nx, Ny, Nz))
+      self.tauxy=stress_initial_cond[4]
+      self.tauxz=stress_initial_cond[5]
+      self.tauyz=stress_initial_cond[6]
 
-radios = 0.228542449538
-indices = cp.sqrt(x**2 + y**2 + z**2) < radios
-E[indices] = E0n
-nu[indices] = nu0n
-alp[indices] = alp0n
+      self.Ux=U_initial_cond[0]
+      self.Uy=U_initial_cond[1]
+      self.Uz=U_initial_cond[2]
 
-K = E / (3.0 * (1 - 2 * nu))
-G = E / (2.0 + 2.0 * nu)
+      self.Vx=V_initial_cond[0]
+      self.Vy=V_initial_cond[1]
+      self.Vz=V_initial_cond[2]
 
-# INITIAL CONDITIONS
-P0 = cp.zeros((Nx, Ny, Nz))
-Ux = cp.zeros((Nx + 1, Ny, Nz))
-Uy = cp.zeros((Nx, Ny + 1, Nz))
-Uz = cp.zeros((Nx, Ny, Nz + 1))
+      self.T=T
 
-Vx = cp.zeros((Nx + 1, Ny, Nz))
-Vy = cp.zeros((Nx, Ny + 1, Nz))
-Vz = cp.zeros((Nx, Ny, Nz + 1))
+  def set_boundary_cond(self, strain_bound_cond ):
+      self.dUxdx=strain_bound_cond[0]
+      self.dUydy=strain_bound_cond[1]
+      self.dUzdz=strain_bound_cond[2]
 
-tauxx = cp.zeros((Nx, Ny, Nz))
-tauyy = cp.zeros((Nx, Ny, Nz))
-tauzz = cp.zeros((Nx, Ny, Nz))
+      self.dUxdy=strain_bound_cond[3]
+      self.dUxdz=strain_bound_cond[4]
+      self.dUydz=strain_bound_cond[5]
+  @timer
+  def find_Keff(self):
+      self.__solving_equation()
+      Keff = cp.mean(-self.P) / (self.dUxdx + self.dUydy + self.dUzdz)
+      print(f'Keff={Keff}')
+      return Keff
 
-tauxy = cp.zeros((Nx - 1, Ny - 1, Nz))
-tauxz = cp.zeros((Nx - 1, Ny, Nz - 1))
-tauyz = cp.zeros((Nx, Ny - 1, Nz - 1))
+  @timer
+  def find_alpha(self, Keff):
+      self.__solving_equation()
+      rr=cp.mean(self.P)
+      alpha = rr / Keff / self.T / 2
+      print(f'Alpha={alpha}')
+      return alpha
 
-# BOUNDARY CONDITIONS
-loadValue = 0.002
-loadType=[1, 0, 0, 0, 0, 0]
+  def __av4_xy(self,A):
+      return 0.25 * (A[:-1, :-1, :] + A[:-1, 1:, :] + A[1:, :-1, :] + A[1:, 1:, :])
+  def __av4_xz(self,A):
+      return 0.25*(A[:-1, :, :-1] + A[:-1, :, 1:] + A[1:, :, :-1] + A[1:, :, 1:])
+  def __av4_yz(self,A):
+      return 0.25*(A[:, :-1, :-1] + A[:, :-1, 1:] + A[:, 1:, :-1] + A[:, 1:, 1:])
 
-dUxdx = loadValue * loadType[0]
-dUydy = loadValue * loadType[1]
-dUzdz = loadValue * loadType[2]
+  def __solving_equation(self):
+      self.Ux += self.dUxdx * self.xUx + self.dUxdy * self.yUx
+      self.Uy += self.dUydy * self.yUy
+      self.Uz += self.dUzdz * self.zUz
 
-dUxdy = loadValue * loadType[3]
-dUxdz = loadValue * loadType[4]
-dUydz = loadValue * loadType[5]
+      for it in range(self.Nt):
+          # displacement divergence
+          divU = cp.diff(self.Ux, axis=0) / self.dX + cp.diff(self.Uy, axis=1) / self.dY + cp.diff(self.Uz, axis=2) / self.dZ
 
-Ux += dUxdx * xUx + dUxdy * yUx
-Uy += dUydy * yUy
-Uz += dUzdz * zUz
+          # constitutive equation - Hooke's law
+          self.P = self.P0 - self.K * divU
 
-# ACTION LOOP
-for it in range(Nt):
-    # displacement divergence
-    divU = cp.diff(Ux, axis=0) / dX + cp.diff(Uy, axis=1) / dY + cp.diff(Uz, axis=2) / dZ
+          self.tauxx = 2.0 * self.G * (cp.diff(self.Ux, axis=0) / self.dX - divU/3.0)
+          self.tauyy = 2.0 * self.G * (cp.diff(self.Uy, axis=1) / self.dY - divU/3.0)
+          self.tauzz = 2.0 * self.G * (cp.diff(self.Uz, axis=2) / self.dZ - divU/3.0)
 
-    # constitutive equation - Hooke's law
-    P = P0 - K * divU
+          self.tauxy = self.__av4_xy(self.G) * (cp.diff(self.Ux[1:-1,:,:], axis=1) / self.dY + cp.diff(self.Uy[:,1:-1,:], axis=0) / self.dX)
+          self.tauxz = self.__av4_xz(self.G) * (cp.diff(self.Ux[1:-1,:,:], axis=2) / self.dZ + cp.diff(self.Uz[:,:,1:-1], axis=0) / self.dX)
+          self.tauyz = self.__av4_yz(self.G) * (cp.diff(self.Uy[:,1:-1,:], axis=2) / self.dZ + cp.diff(self.Uz[:,:,1:-1], axis=1) / self.dY)
 
-    tauxx = 2.0 * G * (cp.diff(Ux, axis=0) / dX - divU/3.0)
-    tauyy = 2.0 * G * (cp.diff(Uy, axis=1) / dY - divU/3.0)
-    tauzz = 2.0 * G * (cp.diff(Uz, axis=2) / dZ - divU/3.0)
+          # motion equation
+          dVxdt = (cp.diff(-self.P[:,1:-1,1:-1] + self.tauxx[:,1:-1,1:-1], axis=0) / self.dX +
+                  cp.diff(self.tauxy[:,:,1:-1], axis=1) / self.dY +
+                  cp.diff(self.tauxz[:,1:-1,:], axis=2) / self.dZ) / self.rho
+          self.Vx[1:-1,1:-1,1:-1] = self.Vx[1:-1,1:-1,1:-1] * (1 - self.dt * self.damp) + dVxdt * self.dt
 
-    tauxy = av4_xy(G) * (cp.diff(Ux[1:-1,:,:], axis=1) / dY + cp.diff(Uy[:,1:-1,:], axis=0) / dX)
-    tauxz = av4_xz(G) * (cp.diff(Ux[1:-1,:,:], axis=2) / dZ + cp.diff(Uz[:,:,1:-1], axis=0) / dX)
-    tauyz = av4_yz(G) * (cp.diff(Uy[:,1:-1,:], axis=2) / dZ + cp.diff(Uz[:,:,1:-1], axis=1) / dY)
+          dVydt = (cp.diff(self.tauxy[:,:,1:-1], axis=0) / self.dX +
+                  cp.diff(-self.P[1:-1,:,1:-1] + self.tauyy[1:-1,:,1:-1], axis=1) / self.dY +
+                  cp.diff(self.tauyz[1:-1,:,:], axis=2) / self.dZ) / self.rho
+          self.Vy[1:-1,1:-1,1:-1] = self.Vy[1:-1,1:-1,1:-1] * (1 - self.dt * self.damp) + dVydt * self.dt
 
-    # motion equation
-    dVxdt = (cp.diff(-P[:,1:-1,1:-1] + tauxx[:,1:-1,1:-1], axis=0) / dX +
-             cp.diff(tauxy[:,:,1:-1], axis=1) / dY +
-             cp.diff(tauxz[:,1:-1,:], axis=2) / dZ) / rho
-    Vx[1:-1,1:-1,1:-1] = Vx[1:-1,1:-1,1:-1] * (1 - dt * damp) + dVxdt * dt
+          dVzdt = (cp.diff(self.tauxz[:,1:-1,:], axis=0) / self.dX +
+                  cp.diff(self.tauyz[1:-1,:,:], axis=1) / self.dY +
+                  cp.diff(-self.P[1:-1,1:-1,:] + self.tauzz[1:-1,1:-1,:], axis=2) / self.dZ) / self.rho
+          self.Vz[1:-1,1:-1,1:-1] = self.Vz[1:-1,1:-1,1:-1] * (1 - self.dt * self.damp) + dVzdt * self.dt
 
-    dVydt = (cp.diff(tauxy[:,:,1:-1], axis=0) / dX +
-             cp.diff(-P[1:-1,:,1:-1] + tauyy[1:-1,:,1:-1], axis=1) / dY +
-             cp.diff(tauyz[1:-1,:,:], axis=2) / dZ) / rho
-    Vy[1:-1,1:-1,1:-1] = Vy[1:-1,1:-1,1:-1] * (1 - dt * damp) + dVydt * dt
+          # displacements
+          self.Ux = self.Ux + self.Vx * self.dt
+          self.Uy = self.Uy + self.Vy * self.dt
+          self.Uz = self.Uz + self.Vz * self.dt
 
-    dVzdt = (cp.diff(tauxz[:,1:-1,:], axis=0) / dX +
-             cp.diff(tauyz[1:-1,:,:], axis=1) / dY +
-             cp.diff(-P[1:-1,1:-1,:] + tauzz[1:-1,1:-1,:], axis=2) / dZ) / rho
-    Vz[1:-1,1:-1,1:-1] = Vz[1:-1,1:-1,1:-1] * (1 - dt * damp) + dVzdt * dt
+def main():
+    Lx = 1.0
+    Ly = 1.0
+    Lz = 1.0
+    size=[Lx,Ly,Lz]
 
-    # displacements
-    Ux = Ux + Vx * dt
-    Uy = Uy + Vy * dt
-    Uz = Uz + Vz * dt
+    # NUMERICS
+    Nx = 100
+    Ny = 100
+    Nz = 100
+    Nt = 1000
+    grid_points=[Nx, Ny, Nz, Nt]
+    CFL = 0.25
 
-Keff = cp.mean(-P) / (dUxdx + dUydy + dUzdz)
-print(f'Keff={Keff}')
+    #Material
+    rho=1.0
+    E0p=1.0
+    nu0p=0.4
+    alp0p=7.7e-5
+    #hole
+    # E0n=0.0
+    # nu0n=0.0
+    # alp0n=0.0
+    E0n = 10.0
+    nu0n = 0.25
+    alp0n = 1.3e-5
 
-end_numpy = time.time()
-time_numpy = end_numpy - start_numpy
-print(f"Время вычисление Keff с cupy: {time_numpy} секунд")
+    radios= 0.228542449538
 
-#
-start_numpy = time.time()
-# INITIAL CONDITIONS
-T = 1.0
-P0 = K * alp * 2 * T
+    # INITIAL CONDITIONS
+    P0 = cp.zeros((Nx, Ny, Nz))
+    Ux = cp.zeros((Nx + 1, Ny, Nz))
+    Uy = cp.zeros((Nx, Ny + 1, Nz))
+    Uz = cp.zeros((Nx, Ny, Nz + 1))
 
-Ux = cp.zeros((Nx + 1, Ny, Nz))  # displacement
-Uy = cp.zeros((Nx, Ny + 1, Nz))
-Uz = cp.zeros((Nx, Ny, Nz + 1))
+    Vx = cp.zeros((Nx + 1, Ny, Nz))
+    Vy = cp.zeros((Nx, Ny + 1, Nz))
+    Vz = cp.zeros((Nx, Ny, Nz + 1))
 
-Vx = cp.zeros((Nx + 1, Ny, Nz))  # velocity
-Vy = cp.zeros((Nx, Ny + 1, Nz))
-Vz = cp.zeros((Nx, Ny, Nz + 1))
+    tauxx = cp.zeros((Nx, Ny, Nz))
+    tauyy = cp.zeros((Nx, Ny, Nz))
+    tauzz = cp.zeros((Nx, Ny, Nz))
 
-tauxx = cp.zeros((Nx, Ny, Nz))  # deviatoric stress
-tauyy = cp.zeros((Nx, Ny, Nz))
-tauzz = cp.zeros((Nx, Ny, Nz))
+    tauxy = cp.zeros((Nx - 1, Ny - 1, Nz))
+    tauxz = cp.zeros((Nx - 1, Ny, Nz - 1))
+    tauyz = cp.zeros((Nx, Ny - 1, Nz - 1))
 
-tauxy = cp.zeros((Nx - 1, Ny - 1, Nz))
-tauxz = cp.zeros((Nx - 1, Ny, Nz - 1))
-tauyz = cp.zeros((Nx, Ny - 1, Nz - 1))
+    stress_initial_cond = [P0, tauxx, tauyy, tauzz, tauxy, tauxz, tauyz]
 
-# BOUNDARY CONDITIONS
-loadValue = 0.0
-loadType = [1, 0, 0, 0, 0, 0]
+    U_initial_cond = [Ux, Uy, Uz]
+    V_initial_cond = [Vx, Vy, Vz]
 
-dUxdx = loadValue * loadType[0]
-dUydy = loadValue * loadType[1]
-dUzdz = loadValue * loadType[2]
+    T = 1.0
+    # BOUNDARY CONDITIONS
+    loadValue = 0.002
+    loadType=[1, 0, 0, 0, 0, 0]
 
-dUxdy = loadValue * loadType[3]
-dUxdz = loadValue * loadType[4]
-dUydz = loadValue * loadType[5]
+    dUxdx = loadValue * loadType[0]
+    dUydy = loadValue * loadType[1]
+    dUzdz = loadValue * loadType[2]
 
-Ux = Ux + (dUxdx * xUx + dUxdy * yUx)
-Uy = Uy + dUydy * yUy
-Uz = Uz + dUzdz * zUz
+    dUxdy = loadValue * loadType[3]
+    dUxdz = loadValue * loadType[4]
+    dUydz = loadValue * loadType[5]
 
-# ACTION LOOP
-for it in range(1, Nt+1):
-    # displacement divergence
-    divU = cp.diff(Ux, axis=0) / dX + cp.diff(Uy, axis=1) / dY + cp.diff(Uz, axis=2) / dZ
+    strain_bound_cond=[dUxdx, dUydy, dUzdz, dUxdy, dUxdz, dUydz]
 
-    # constitutive equation - Hooke's law
-    P = P0 - K * divU
+    #
+    ob=eff_prop3D(size, grid_points, CFL, rho, E0p, nu0p, alp0p, E0n, nu0n, alp0n, radios,
+            stress_initial_cond, U_initial_cond, V_initial_cond, T, strain_bound_cond)
 
-    tauxx = 2.0 * G * (cp.diff(Ux, axis=0) / dX - divU/3.0)
-    tauyy = 2.0 * G * (cp.diff(Uy, axis=1) / dY - divU/3.0)
-    tauzz = 2.0 * G * (cp.diff(Uz, axis=2) / dZ - divU/3.0)
+    Keff=ob.find_Keff()
+    #
+    #T=1.0
+    P0 = ob.K * ob.alp * 2 * ob.T
+    Ux = cp.zeros((Nx + 1, Ny, Nz))
+    Uy = cp.zeros((Nx, Ny + 1, Nz))
+    Uz = cp.zeros((Nx, Ny, Nz + 1))
 
-    tauxy = av4_xy(G) * (cp.diff(Ux[1:-1,:,:], axis=1) / dY + cp.diff(Uy[:,1:-1,:], axis=0) / dX)
-    tauxz = av4_xz(G) * (cp.diff(Ux[1:-1,:,:], axis=2) / dZ + cp.diff(Uz[:,:,1:-1], axis=0) / dX)
-    tauyz = av4_yz(G) * (cp.diff(Uy[:,1:-1,:], axis=2) / dZ + cp.diff(Uz[:,:,1:-1], axis=1) / dY)
+    Vx = cp.zeros((Nx + 1, Ny, Nz))
+    Vy = cp.zeros((Nx, Ny + 1, Nz))
+    Vz = cp.zeros((Nx, Ny, Nz + 1))
 
-    # motion equation
-    dVxdt = (cp.diff(-P[:,1:-1,1:-1] + tauxx[:,1:-1,1:-1], axis=0) / dX +
-             cp.diff(tauxy[:,:,1:-1], axis=1) / dY +
-             cp.diff(tauxz[:,1:-1,:], axis=2) / dZ) / rho
-    Vx[1:-1,1:-1,1:-1] = Vx[1:-1,1:-1,1:-1] * (1 - dt * damp) + dVxdt * dt
+    tauxx = cp.zeros((Nx, Ny, Nz))
+    tauyy = cp.zeros((Nx, Ny, Nz))
+    tauzz = cp.zeros((Nx, Ny, Nz))
 
-    dVydt = (cp.diff(tauxy[:,:,1:-1], axis=0) / dX +
-             cp.diff(-P[1:-1,:,1:-1] + tauyy[1:-1,:,1:-1], axis=1) / dY +
-             cp.diff(tauyz[1:-1,:,:], axis=2) / dZ) / rho
-    Vy[1:-1,1:-1,1:-1] = Vy[1:-1,1:-1,1:-1] * (1 - dt * damp) + dVydt * dt
+    tauxy = cp.zeros((Nx - 1, Ny - 1, Nz))
+    tauxz = cp.zeros((Nx - 1, Ny, Nz - 1))
+    tauyz = cp.zeros((Nx, Ny - 1, Nz - 1))
 
-    dVzdt = (cp.diff(tauxz[:,1:-1,:], axis=0) / dX +
-             cp.diff(tauyz[1:-1,:,:], axis=1) / dY +
-             cp.diff(-P[1:-1,1:-1,:] + tauzz[1:-1,1:-1,:], axis=2) / dZ) / rho
-    Vz[1:-1,1:-1,1:-1] = Vz[1:-1,1:-1,1:-1] * (1 - dt * damp) + dVzdt * dt
+    stress_initial_cond = [P0, tauxx, tauyy, tauzz, tauxy, tauxz, tauyz]
 
-    # displacements
-    Ux = Ux + Vx * dt
-    Uy = Uy + Vy * dt
-    Uz = Uz + Vz * dt
+    U_initial_cond = [Ux, Uy, Uz]
+    V_initial_cond = [Vx, Vy, Vz]
+    #
+    ob.set_initial_cond( stress_initial_cond, U_initial_cond, V_initial_cond, T)
+    #
+    loadValue = 0.0
+    loadType=[1, 0, 0, 0, 0, 0]
 
+    dUxdx = loadValue * loadType[0]
+    dUydy = loadValue * loadType[1]
+    dUzdz = loadValue * loadType[2]
 
-alpha = cp.mean(P) / Keff / T / 2
-print(f'alpha={alpha}')
+    dUxdy = loadValue * loadType[3]
+    dUxdz = loadValue * loadType[4]
+    dUydz = loadValue * loadType[5]
 
-end_numpy = time.time()
-time_numpy = end_numpy - start_numpy
-print(f"Время вычисление alpha с cupy: {time_numpy} секунд")
+    strain_bound_cond=[dUxdx, dUydy, dUzdz, dUxdy, dUxdz, dUydz]
+    #
+    ob.set_boundary_cond(strain_bound_cond)
 
+    al=ob.find_alpha(Keff)
+
+if __name__ == "__main__":
+    main()
